96f8f8e18ca2150c53dce3a8b519e333bfe86081
nff825 <2508979491@qq.com>
2023-10-11 12:04:40 +0800

    Site updated: 2023-10-11 12:04:40

---
+++ js/fold_tag.js
@@ -0,0 +1,21 @@
+/* global hexo */
+
+<script src="js/fold_tag.js"></script>
+hexo.extend.tag.register('fold', function(args, content) {
+  var text = args.join(' ');
+  if(!text) text = "点击显示/隐藏";
+  return '<div><div class="fold_hider"><div class="close hider_title">'
+    + hexo.render.renderSync({text: text, engine: 'markdown'}).replace(/^<p>/, '').replace(/<\/p>$/, '');
+});
+
+
+function fold(args, content) {
+  var text = args.join(' ');
+  if(!text) text = "点击显示/隐藏";
+  return '<div><div class="fold_hider"><div class="close hider_title">'
+    + hexo.render.renderSync({text: text, engine: 'markdown'}).replace(/^<p>/, '').replace(/<\/p>$/, '')
+    + '</div></div><div class="fold">\n'
+    + hexo.render.renderSync({text: content, engine: 'markdown'})
+    + '\n</div></div>';
+}
+hexo.extend.tag.register('fold', fold, {ends: true});
